7d84b7e16f580e5ae4ba65af990ee53a7b30c102
aurora christensen <aurora.christensen@gmail.com>
2022-03-18 22:04:06 -0700

    adding circle marker to dark mode map

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -6,7 +6,7 @@ console.log("working");
 with setView() method:
 CreateS the map object with a (center [lat,lng] and zoom level).
 'mapid' references the id tag in html file */
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 /* OR create map with each attribute modification
 
@@ -16,6 +16,13 @@ let map = L.map("mapid", {
 });
 
 */
+
+// 13.4.1  Add a marker to the map for Los Angeles, California.
+L.circleMarker([34.0522, -118.2437], {
+  radius: 300,
+  color: "black",
+  fillColor: "#ffffa1"
+}).addTo(map);
 
 /* ADD TILE LAYER
 OPTION 1: use tileLayer() method and leaflet code from quickstart guide */
@@ -39,8 +46,8 @@ mapbox/satellite-v9
 mapbox/satellite-streets-v11 */
 
 //OPTION 2: Use Mapbox styles api for "static tiles api"
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// We create the title layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
