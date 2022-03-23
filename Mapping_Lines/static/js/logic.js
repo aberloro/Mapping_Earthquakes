1a5943b84e2c0df6cd78ef73531da89b730f8214
aurora christensen <aurora.christensen@gmail.com>
2022-03-22 18:23:36 -0700

    adding style to mapping lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -14,27 +14,17 @@ let line = [
   [47.4502, -122.3088]
 ];
 
-// Create a yellow polyline using the line coordinates 
+// Create a blue dashed line polyline using the line coordinates 
 L.polyline(line, {
-  color: "yellow"
+  color: "blue",
+  dashArray: 5,
+  opacity: .5,
+  weight: 2
 }).addTo(map);
-
-/* Get data from cities.js
-let cityData = cities;
-
- Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-  console.log(city)
-  L.circleMarker(city.location, {
-    radius: city.population/100000
-  })
-  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-.addTo(map);
-}); */
 
 /*Use Mapbox styles api for "static tiles api" to
 We create the tile layer that will be the background of our map.*/
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
