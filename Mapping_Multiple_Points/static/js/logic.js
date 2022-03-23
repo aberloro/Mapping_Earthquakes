e706b056f4f1dac3e667aee9b6bd178d1103d271
aurora christensen <aurora.christensen@gmail.com>
2022-03-22 18:08:09 -0700

    Styling multiple points

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,8 +2,7 @@
 console.log("working");
 
 /* ADD MAP OBJECT with setView() method:
-CreateS the map object with a (center [lat,lng] and zoom level).
-'mapid' references the id tag in html file */
+CreateS the map object with a (center [lat,lng] and zoom level).*/
 let map = L.map('mapid').setView([34.0522, -100.2437], 4);
 
 // Get data from cities.js
@@ -13,19 +12,21 @@ let cityData = cities;
 cityData.forEach(function(city) {
   console.log(city)
   L.circleMarker(city.location, {
-    radius: city.population/100000
+    radius: city.population/100000,
+    color: "DeepPink",
+    weight: 1
   })
   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-.addTo(map);
+  .addTo(map);
 });
 
 /*Use Mapbox styles api for "static tiles api" to
 We create the tile layer that will be the background of our map.*/
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
+  attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  accessToken: API_KEY
 });
 
-// Then we add our 'graymap' tile layer to the map.
+// Then we add our tile layer to the map.
 streets.addTo(map);
