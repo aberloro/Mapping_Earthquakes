5e7e7b3fa287f1ad503279b796549d46f885905d
aurora christensen <aurora.christensen@gmail.com>
2022-03-23 17:39:02 -0700

    adding major earthquake layer for deliverable 2

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -31,11 +31,13 @@ let baseMaps = {
 // 1. Add a 2nd layer group for the tectonic plate data.
 let allEarthquakes = new L.LayerGroup();
 let tectonicPlates = new L.LayerGroup();
+let majorEQ = new L.LayerGroup();
 
 // 2. Add a reference to the tectonic plates group to the overlays object.
 let overlays = {
   "Tectonnic Plates": tectonicPlates,
-  "Earthquakes": allEarthquakes
+  "Earthquakes": allEarthquakes,
+  "Major Earthquakes": majorEQ
 };
 
 // Then we add a control to the map that will allow the user to change which
@@ -73,9 +75,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   // This function determines the radius of the earthquake marker based on its magnitude.
   // Earthquakes with a magnitude of 0 were being plotted with the wrong radius.
   function getRadius(magnitude) {
-    if (magnitude === 0) {
-      return 1;
-    }
+    if (magnitude === 0) {return 1;}
     return magnitude * 4;
   }
 
@@ -98,6 +98,66 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   // Then we add the earthquake layer to our map.
   allEarthquakes.addTo(map);
 });
+
+////////////////////////////Deliverable 2//////////////////////////
+
+// 3. Retrieve the major earthquake GeoJSON data >4.5 mag for the week.
+let majorEQData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson"
+
+d3.json(majorEQData).then(function(data) {
+
+  // 4. Use the same style as the earthquake data.
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: .7,
+      fillColor: getColor(feature.properties.mag),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+  
+  // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
+  function getColor(magnitude) {
+    if (magnitude > 6) {return "#6925c2";}
+    if (magnitude > 5) {return "#ea2c2c";}
+    return "#ea822c";
+  }
+  
+  // 6. Use the function that determines the radius of the earthquake marker based on its magnitude.
+  function getRadius(magnitude) {
+    if (magnitude === 0) {return 1;}
+    return magnitude * 4;
+  }
+  
+  // 7. Creating a GeoJSON layer with the retrieved data that adds a circle to the map 
+  // sets the style of the circle, and displays the magnitude and location of the earthquake
+  //  after the marker has been created and styled.
+  L.geoJson(data, {
+    // We turn each feature into a circleMarker on the map.
+    pointToLayer: function(feature, latlng) {
+      console.log(data);
+      return L.circleMarker(latlng);
+    },
+    // We set the style for each circleMarker using our styleInfo function.
+    style: styleInfo,
+    // We create a popup for each circleMarker to display the magnitude and location of the earthquake
+    //  after the marker has been created and styled.
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+ 
+  // 8. Add the major earthquakes layer to the map.
+  }).addTo(majorEQ);majorEQ.addTo(map);
+  
+  // 9. Close the braces and parentheses for the major earthquake data.
+});
+  
+///////////////////////////End Deliverable 2///////////////////////
+
+
 
 // Here we create a legend control object.
 let legend = L.control({position: "bottomright"});
@@ -128,6 +188,8 @@ let legend = L.control({position: "bottomright"});
 
 // Finally, we our legend to the map.
 legend.addTo(map);
+
+/////////////////////Deliverable 1///////////////////////////////////
 
 // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
 let tectonicPlatesData = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
